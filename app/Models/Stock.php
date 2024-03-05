a2078a30c814c02e85f2e292520bdb1ecdf132a7
antonyevchik <av.yevchik@gmail.com>
2024-03-05 10:15:35 +0200

    Product Tracking

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -4,10 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
 
 class Stock extends Model
 {
     use HasFactory;
 
     protected $table = 'stock';
+
+    protected $casts = [
+        'in_stock' => 'boolean'
+    ];
+
+    public function track()
+    {
+        if ($this->retailer->name === 'Best Buy') {
+            $results = Http::get('http://foo.test')->json();
+
+            $this->update([
+                'in_stock' => $results['available'],
+                'price' => $results['price']
+            ]);
+        }
+    }
+
+    public function retailer()
+    {
+        return $this->belongsTo(Retailer::class);
+    }
 }
