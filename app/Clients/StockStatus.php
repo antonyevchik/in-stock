6b00bf99f57f4c299084e3a57ebd67fcf36f9431
antonyevchik <av.yevchik@gmail.com>
2024-03-13 21:28:41 +0200

    Techniques for testing against a real api

--- app/Clients/StockStatus.php
+++ app/Clients/StockStatus.php
@@ -5,10 +5,10 @@ namespace App\Clients;
 class StockStatus
 {
 
-    public $available;
-    public $price;
+    public bool $available;
+    public int $price;
 
-    public function __construct($available, $price)
+    public function __construct(bool $available, int $price)
     {
         $this->available = $available;
         $this->price     = $price;
