b6a2a52c036f275eb47dd201b407c18b944ee9b0
antonyevchik <av.yevchik@gmail.com>
2024-03-18 12:19:20 +0200

    Refactoring to use case p1 (11:57)

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Events\NowInStock;
+use App\UseCases\TrackStock;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,22 +16,9 @@ class Stock extends Model
         'in_stock' => 'boolean',
     ];
 
-    public function track($callback = null)
+    public function track()
     {
-        $status = $this->retailer
-            ->client()
-            ->checkAvailability($this);
-
-        if(!$this->in_stock && $status->available) {
-            event(new NowInStock($this));
-        }
-
-        $this->update([
-            'in_stock' => $status->available,
-            'price'    => $status->price,
-        ]);
-
-        $callback && $callback($this);
+        (new TrackStock($this))->handle();
     }
 
     public function retailer()
